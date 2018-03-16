3b22a28a5aaf4209473b791476d091fd665fff97
houstonlucas <houstonlucas@nevada.unr.edu>
2018-03-16 16:48:27 -0700

    Restructuring Primary Window class.

--- src/com/company/MineDusterPrimaryWindow.java
+++ src/com/company/MineDusterPrimaryWindow.java
@@ -1,17 +1,63 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class MineDusterPrimaryWindow {
-    public MineDusterPrimaryWindow() {
+
+    private JPanel PrimaryPanel;
+    private JPanel GamePanel;
+    private JPanel ButtonPanel;
+    private JLabel TimerLabel;
+    private JLabel MineCountLabel;
+    private JButton ResetButton;
+    private JButton PauseButton;
+    private Minefield minefield;
+    private Properties properties;
+    private Dimension fieldSize;
+
+    private boolean gameStarted;
+    private boolean gamePaused;
+
+    public MineDusterPrimaryWindow(int num_mines, int field_height, int field_width) {
+        minefield = new Minefield(num_mines, field_height, field_width);
+        gamePaused = true;
+        gameStarted = false;
+
+
+        properties = new Properties();
+        try {
+            properties.load(new FileInputStream("res/strings.properties"));
+        } catch (IOException e) {
+        }
+
+        setActionListeners();
+
+    }
+
+    private void setActionListeners(){
+
+
         PauseButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String newText;
+                if(gamePaused){
+                    newText = properties.getProperty("pauseButtonText");
+                }else{
+                    newText = properties.getProperty("pauseButtonTextResume");
+                }
+                PauseButton.setText(newText);
+                gamePaused = !gamePaused;
 
             }
         });
+
 
         ResetButton.addActionListener(new ActionListener() {
             @Override
@@ -25,11 +71,4 @@ public class MineDusterPrimaryWindow {
         return PrimaryPanel;
     }
 
-    private JPanel PrimaryPanel;
-    private JPanel GamePanel;
-    private JPanel ButtonPanel;
-    private JLabel TimerLabel;
-    private JLabel MineCountLabel;
-    private JButton ResetButton;
-    private JButton PauseButton;
 }
