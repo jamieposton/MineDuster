1c16e85a4cb2403c7f96cf6044880814a6242e7b
houstonlucas <houstonlucas@nevada.unr.edu>
2018-03-16 16:46:15 -0700

    Setup params for constructor call.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,13 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int num_mines = 10;
+        int field_height = 20;
+        int field_width = 20;
 
         JFrame frame = new JFrame("MineDusterPrimaryWindow");
-        frame.setContentPane(new MineDusterPrimaryWindow().getPrimaryPanel());
+        frame.setContentPane(new MineDusterPrimaryWindow(num_mines, field_height, field_width).getPrimaryPanel());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
