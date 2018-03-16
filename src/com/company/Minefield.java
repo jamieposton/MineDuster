349f5c28ff19c6460c6f505765ad5c16303de17f
jamieposton <jposton@nevada.unr.edu>
2018-03-16 13:05:29 -0700

    Created Minefield Constructor

--- src/com/company/Minefield.java
+++ src/com/company/Minefield.java
@@ -1,4 +1,21 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Minefield {
+    private ArrayList<ArrayList<FieldSpace>> field;
+    private int num_mines;
+
+    public Minefield(int number_mines, int height, int width) {
+        this.num_mines = number_mines;
+
+        for(int i = 0; i < height; i++) {
+            ArrayList<FieldSpace> temp = new ArrayList<>();
+            for(int j = 0; j < width; j++) {
+                FieldSpace tempSpace = new FieldSpace(false, false, false, 0);
+                temp.add(tempSpace);
+            }
+            this.field.add(temp);
+        }
+    }
 }
