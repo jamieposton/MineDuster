8853fba809b6f986af7a29a3c63655c735c6639d
houstonlucas <houstonlucas@nevada.unr.edu>
2018-03-16 13:49:05 -0700

    Call gui from main.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,16 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
+        JFrame frame = new JFrame("MineDusterPrimaryWindow");
+        frame.setContentPane(new MineDusterPrimaryWindow().getPrimaryPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
