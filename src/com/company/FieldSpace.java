fd939e4d449adba6863a43ec33344ef526edd43d
jamieposton <jposton@nevada.unr.edu>
2018-03-16 12:56:33 -0700

    Created FieldSpace class

---
+++ src/com/company/FieldSpace.java
@@ -0,0 +1,48 @@
+package com.company;
+
+public class FieldSpace {
+
+    private boolean flag;
+    private boolean mine;
+    private boolean cleared;
+    private int warnings;
+
+    public FieldSpace(boolean flag, boolean mine, boolean cleared, int warnings) {
+        this.flag = flag;
+        this.mine = mine;
+        this.cleared = cleared;
+        this.warnings = warnings;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public boolean isMine() {
+        return mine;
+    }
+
+    public void setMine(boolean mine) {
+        this.mine = mine;
+    }
+
+    public boolean isCleared() {
+        return cleared;
+    }
+
+    public void setCleared(boolean cleared) {
+        this.cleared = cleared;
+    }
+
+    public int getWarnings() {
+        return warnings;
+    }
+
+    public void incrementWarnings() {
+        warnings++;
+    }
+}
