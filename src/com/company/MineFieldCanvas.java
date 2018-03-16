39b1473488d2e8c341f30d6ef8d350886f681750
houstonlucas <houstonlucas@nevada.unr.edu>
2018-03-16 16:46:37 -0700

    Created Custom MineFieldCanvas class. (Not working)

---
+++ src/com/company/MineFieldCanvas.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MineFieldCanvas extends JPanel {
+    MineFieldCanvas(){
+        super(true);
+    }
+
+    public void createUIComponents(){
+
+    }
+
+    public void paint(Graphics g){
+
+    }
+}
