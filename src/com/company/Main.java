05ab1370f4ef56414a1823b06133f154f631ef45
houstonlucas <houstonlucas@nevada.unr.edu>
2018-03-16 18:37:13 -0700

    Added Sizing info for canvas. Uses Magic numbers.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,33 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
         int num_mines = 10;
-        int field_height = 20;
-        int field_width = 20;
+        int fieldHeight = 20;
+        int fieldWidth = 20;
+
+        int fieldSpaceSize = 30;
+        int gameAreaHeight = fieldHeight * fieldSpaceSize;
+        int gameAreaWidth = fieldWidth * fieldSpaceSize;
+
+        //TODO remove these magic numbers
+        int windowHeight = gameAreaHeight-9;
+        int buttonPanelWidth = 100;
+        int windowWidth = gameAreaWidth + buttonPanelWidth-19;
+
+        Dimension windowSize = new Dimension(windowWidth, windowHeight);
 
         JFrame frame = new JFrame("MineDusterPrimaryWindow");
-        frame.setContentPane(new MineDusterPrimaryWindow(num_mines, field_height, field_width).getPrimaryPanel());
+        frame.setPreferredSize(windowSize);
+        frame.setContentPane(new MineDusterPrimaryWindow(num_mines, fieldHeight, fieldWidth, windowSize).getPrimaryPanel());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        frame.setResizable(false);
         frame.setVisible(true);
     }
 }
