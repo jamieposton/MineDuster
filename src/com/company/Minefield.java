c5d24118a884b5a498ea5cb81f490ff2bd18453d
jamieposton <jposton@nevada.unr.edu>
2018-03-19 14:39:08 -0700

    Commented Minefield class

--- src/com/company/Minefield.java
+++ src/com/company/Minefield.java
@@ -19,6 +19,9 @@ public class Minefield {
 
     public enum GAME_STATE{GAME_WON, GAME_LOST, GAME_ONGOING}
 
+    //Constructor for the minefield.
+    //DOES NOT generate all the mines and stuff
+    //The function generate field needs to be called seperately to make the mines and warnings.
     Minefield(int number_mines, int new_height, int new_width) {
         numMines = number_mines;
         height = new_height;
@@ -43,6 +46,8 @@ public class Minefield {
         }
     }
 
+    //Generates all the mines and warnings.
+    //The initial point passed in will automatically be set to clear.
     public void generateField(int initX, int initY) {
         int mines = 0;
         Random rand = new Random();
@@ -65,6 +70,7 @@ public class Minefield {
         startTime = System.currentTimeMillis();
     }
 
+    //Increments all warnings adjacent to the parameter point
     private void addWarnings(int x, int y) {
         for(int i = -1; i < 2; i++) {
             for(int j = -1; j < 2; j++) {
@@ -75,10 +81,13 @@ public class Minefield {
         }
     }
 
+    //Checks if the point is in the bounds of the field or not.
     private boolean isInBounds(int x, int y) {
         return x >= 0 && x < width && y >= 0 && y < height;
     }
 
+    //Adds a flag to a point on the field
+    //Increments/Decrements total flags
     public boolean addFlag(int x, int y) {
         if( !isInBounds(x, y) ) {
             return false;
@@ -96,7 +105,8 @@ public class Minefield {
         }
     }
 
-    private int getNumFlags(int x, int y) {
+    //Gets the amount of flags adjacent to a point
+    private int getNumNeighborFlags(int x, int y) {
         int totalFlags = 0;
         for (int i = -1; i < 2; i++) {
             for (int j = -1; j < 2; j++) {
@@ -108,13 +118,16 @@ public class Minefield {
         return totalFlags;
     }
 
+    //Clears a space
     public void clearSpace(int x, int y) {
+        //If the space was a mine, switch game state to lost
         if(field.get(y).get(x).isMine()) {
             gameState = GAME_STATE.GAME_LOST;
             return;
         }
 
-        if(field.get(y).get(x).isCleared() && field.get(y).get(x).getWarnings() == getNumFlags(x, y)) {
+        //If the space was already cleared and has the correct flags, clear all uncleared neighbors
+        if(field.get(y).get(x).isCleared() && field.get(y).get(x).getWarnings() == getNumNeighborFlags(x, y)) {
             for (int i = -1; i < 2; i++) {
                 for (int j = -1; j < 2; j++) {
                     if( isInBounds(x+i, y+j) && !(i == 0 && j == 0) && !field.get(y+j).get(x+i).isCleared()) {
@@ -123,7 +136,9 @@ public class Minefield {
                 }
             }
         }
+
         else {
+            //Clear the space
             Point currentPoint = new Point(x, y);
             Stack<Point> ToCheck = new Stack<>();
             ToCheck.push(currentPoint);
@@ -131,6 +146,7 @@ public class Minefield {
                 currentPoint = ToCheck.pop();
                 field.get(currentPoint.y).get(currentPoint.x).setCleared(true);
 
+                //If the space was a 0, flood fill everything touching the 0
                 if (field.get(currentPoint.y).get(currentPoint.x).getWarnings() <= 0) {
 
                     for (int i = -1; i < 2; i++) {
@@ -145,9 +161,11 @@ public class Minefield {
                 }
             }
         }
+        //And after all that, check to see if we've won or not
         gameState = isWon();
     }
 
+    //Checks to see if all the non mines are cleared or not
     private GAME_STATE isWon() {
         for( int i = 0; i < width; i++ ) {
             for( int j = 0; j < height; j++) {
@@ -156,9 +174,11 @@ public class Minefield {
                 }
             }
         }
+        //If everything possible is cleared, we've won
         return GAME_STATE.GAME_WON;
     }
 
+    //Resets the field to get ready to generate a new one.
     public void reset() {
         for( int i = 0; i < width; i++ ) {
             for( int j = 0; j < height; j++ ) {
@@ -175,6 +195,7 @@ public class Minefield {
         gameStarted = false;
     }
 
+    //Pauses the time counter and saves the total time.
     public void pause() {
         if( gameState == GAME_STATE.GAME_ONGOING )
         {
@@ -190,6 +211,7 @@ public class Minefield {
         }
     }
 
+    //Prints out the whole field to the terminal for debugging.
     public void printField()
     {
         char flag = 'F';
@@ -216,26 +238,34 @@ public class Minefield {
         }
     }
 
+    //Getter function for the field itself.
+    //Generally used for painting the canvas.
     public ArrayList<ArrayList<FieldSpace>> getField() {
         return field;
     }
 
+    //Getter function for the number of mines in the field.
     public int getNumMines() {
         return numMines;
     }
 
+    //Getter function for the height of the field.
     public int getHeight() {
         return height;
     }
 
+    //Getter function for the width of the field.
     public int getWidth() {
         return width;
     }
 
+    //Getter function for the current total of flags of the field.
     public int getTotalFlags() {
         return totalFlags;
     }
 
+    //Getter function for the current time.
+    //Has different functionality for if the game is paused or not
     public long getTime() {
         if( gamePaused ) {
             return totalTime;
